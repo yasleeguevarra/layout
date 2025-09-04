6791af0d67c49289c16cab945ed9833dc13a7c32
yasleeguevarra <guevarrayaslee20@gmail.com>
2025-09-04 14:13:31 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="container">
+      <div className="header">Header</div>
+      <div className="content">
+        <div className="content1">
+          <div className="c1left">
+            <div className="hero">Hero</div>
+            <div className="sidebar">Sidebar</div>
+          </div>
+          <div className="c1right">
+            <div className="mc">Main Content</div>
+            <div className="ec">Extra Content</div>
+          </div>
+        </div>
+        <div className="content2">
+          <div className="ri">Related Images</div>
+          <div className="rp">Related Posts</div>
+        </div>
+      </div>
+      <div className="footer">Footer</div>
+    </div>
+      
+  )
+}
+
+      export default App
